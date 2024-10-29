222861f1a5cc7950a1f921a23c4a2b324dd501e1
Sidibe8 <syoro4663@gmail.com>
2024-10-29 23:20:56 +0100

    ooooooo

--- index.js
+++ index.js
@@ -6,7 +6,22 @@ const cors = require('cors');
 const app = express();
 
 // Middleware CORS
-app.use(cors());
+var whitelist = ['http://localhost:5500', 'http://localhost:8080']; //white list consumers
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  },
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+};
+
+app.use(cors(corsOptions));
   
 
 // Middleware pour parser le JSON des requêtes
